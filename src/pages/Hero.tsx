6840730bd95e2d8f2e8b080f36763ba88ee8c793
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-08-21 02:21:33 +0100

    worked on the responsiveness of the hero page

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero: React.FC = () => {
   return (
     <div className="font-mont pt-10 md:pt-1">
-      <div className="md:flex md:ml-20 flex-row-reverse">
+      <div className="lg:flex md:ml-20 flex-row-reverse">
         <div className="">
           <img
             src="/images/undraw_indoor_bike_pwa4.png"
@@ -11,15 +11,15 @@ const Hero: React.FC = () => {
             className="w-4/4 md:w-3/4 mt-20 md:mt-20 md:ml-16 ml-8"
           />
         </div>
-        <div className="ml-9 md:ml-1 md:w-1/2 mt-28">
-          <h1 className="text-center  md:ml-1 text-2xl md:text-6xl md:mr-10 font-bold">
+        <div className="ml-9 md:ml-1 lg:w-1/2 mt-28">
+          <h1 className="text-center  lg:ml-1 text-2xl md:text-6xl md:mr-10 font-bold">
             Reach Your Body Goal at FitQuest
           </h1>
-          <p className="text-center mt-7  md:ml-10 md:w-96 text-xl">
+          <p className="text-center mt-7  lg::ml-10 md:w-2/4 md:ml-40 lg:ml-1 lg:w-96 text-xl">
             We believe that the potential to achieve desired health and wellness
             goals lies within everyone.
           </p>
-          <button className="ml-20 py-2 px-20 bg-orange-600 md:ml-44 md:py-2 md:px-7 rounded-full text-white mt-10 hover:bg-orange-400">
+          <button className="ml-20 py-2 px-20 bg-orange-600  whitespace-nowrap md:py-4 md:px-72 lg:ml-44 lg:py-2 lg:px-7 rounded-full text-white mt-10 hover:bg-orange-400">
             Join Now
           </button>
           <div>
