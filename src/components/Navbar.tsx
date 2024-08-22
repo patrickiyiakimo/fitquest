2b83f2d8afa114496044acdef746e5f5d641f3de
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-08-22 12:02:31 +0100

    added a light and dark theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar() {
+  // Initialize theme with explicit string type
+  const [theme, setTheme] = useState<string>(
+    localStorage.getItem("theme") ?? "light"
+  );
+
+
+  const handleToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newTheme = e.target.checked ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+
+    // Safely access document and set attribute
+    const htmlElement = document.querySelector("html");
+    if (htmlElement) {
+      htmlElement.setAttribute("data-theme", theme);
+    }
+  }, [theme]);
+
   return (
-    <div className="navbar bg-base-100 font-mont fixed ">
+    <div className="navbar bg-base-100 font-mont fixed">
       <div className="navbar-start">
-        <a className="btn btn-ghost text-2xl font-bold mr-96 ">
+        <a className="btn btn-ghost text-2xl font-bold mr-96">
           <button>
             <img src="/images/logoipsum-248.svg" alt="logo" />
           </button>
@@ -28,7 +49,32 @@ export default function Navbar() {
             <a>Blogs</a>
           </li>
           <li>
-            <a className=" px-10 rounded-full py-2 bg-black text-white hover:bg-orange-600">Contact</a>
+            <a className="px-10 rounded-full py-2 bg-black text-white hover:bg-orange-600">
+              Contact
+            </a>
+          </li>
+          <li>
+            <label className="swap swap-rotate">
+              <input
+                type="checkbox"
+                checked={theme === "dark"}
+                onChange={handleToggle}
+              />
+              <svg
+                className="swap-on h-10 w-10 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+              </svg>
+              <svg
+                className="swap-off h-10 w-10 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+              </svg>
+            </label>
           </li>
         </ul>
       </div>
@@ -37,7 +83,6 @@ export default function Navbar() {
       <div className="drawer drawer-end lg:hidden">
         <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex justify-end">
-          {/* Page content here */}
           <label
             htmlFor="my-drawer-4"
             className="drawer-button text-orange-600"
@@ -60,7 +105,6 @@ export default function Navbar() {
             className="drawer-overlay"
           ></label>
           <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-10 pt-80">
-            {/* Sidebar content here */}
             <li>
               <a>Home</a>
             </li>
@@ -75,8 +119,33 @@ export default function Navbar() {
             </li>
             <li>
               <div className="navbar-end">
-                <a className="px-10 rounded-full py-2 bg-black text-white hover:bg-orange-600">Button</a>
+                <a className="px-10 rounded-full py-2 bg-black text-white hover:bg-orange-600">
+                  Button
+                </a>
               </div>
+            </li>
+            <li>
+              <label className="swap swap-rotate">
+                <input
+                  type="checkbox"
+                  checked={theme === "dark"}
+                  onChange={handleToggle}
+                />
+                <svg
+                  className="swap-on h-10 w-10 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                </svg>
+                <svg
+                  className="swap-off h-10 w-10 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                </svg>
+              </label>
             </li>
           </ul>
         </div>
