0d8b7a1e25f2fb36859fcb1367fb147b40c4d8bd
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-08-23 01:45:44 +0100

    added routing to the signup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,130 @@
+// import Navbar from "./components/Navbar";
+// import Hero from "./pages/Hero";
+// import Designed from "./pages/Designed";
+// import PersonalTraining from "./pages/PersonalTraining";
+// import Footer from "./components/Footer";
+// import Faq from "./pages/Faq";
+// import SignUp from "./components/SignUp";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Navbar />
+//         <Switch>
+//           <Route exact path="/">
+//             <Hero />
+//             <Designed />
+//             <PersonalTraining />
+//             <Faq />
+//           </Route>
+//           <Route path="/signup">
+//             <SignUp />
+//           </Route>
+//         </Switch>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Hero from "./pages/Hero";
+// import Designed from "./pages/Designed";
+// import PersonalTraining from "./pages/PersonalTraining";
+// import Footer from "./components/Footer";
+// import Faq from "./pages/Faq";
+// import SignUp from "./components/SignUp";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<Hero />} />
+//           <Route path="/designed" element={<Designed />} />
+//           <Route path="/personal-training" element={<PersonalTraining />} />
+//           <Route path="/faq" element={<Faq />} />
+//           <Route path="/signup" element={<SignUp />} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Hero from "./pages/Hero";
+// import Designed from "./pages/Designed";
+// import PersonalTraining from "./pages/PersonalTraining";
+// import Footer from "./components/Footer";
+// import Faq from "./pages/Faq";
+// import SignUp from "./components/SignUp";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Hero />} />
+//         <Route path="/designed" element={<Designed />} />
+//         <Route path="/personal-training" element={<PersonalTraining />} />
+//         <Route path="/faq" element={<Faq />} />
+//         <Route path="/signup" element={<SignUp />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./pages/Hero";
 import Designed from "./pages/Designed";
 import PersonalTraining from "./pages/PersonalTraining";
 import Footer from "./components/Footer";
 import Faq from "./pages/Faq";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <Navbar />
-      </div>
-      <div>
-        <Hero />
-      </div>
-      <div>
-        <Designed />
-      </div>
-      <div>
-        <PersonalTraining />
-      </div>
-      <div>
-        <Faq />
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/designed" element={<Designed />} />
+        <Route path="/personal-training" element={<PersonalTraining />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/SignUp.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function SignUp() {
+  return (
+    <div>
+      SignUp Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+      voluptate corrupti quasi, repudiandae magni illum sit atque? Molestias
+      vitae quod ullam nostrum neque aut nemo voluptas eius ratione magni!
+      Repudiandae vero ducimus, delectus itaque ad deserunt nesciunt eius ipsa
+      dicta, officiis iste, magni enim aperiam sit! Ea optio ad libero nesciunt?
+      Itaque atque dicta blanditiis sit eius ullam harum! Dignissimos vel
+      provident tenetur repellendus quos ipsa aspernatur laudantium doloribus
+      aut nemo deleniti est modi, veniam tempore sit reiciendis. Unde neque
+      nobis numquam maiores earum! Sapiente ullam quisquam, officiis nemo
+      explicabo vel facere libero tempore repudiandae quaerat, suscipit dolor
+      quis odio molestiae natus delectus commodi voluptas perferendis nam.
+      Possimus laudantium doloribus magnam ipsam amet autem, molestias ut ex
+      culpa vitae! Blanditiis eveniet quia quod debitis nulla? Inventore fugiat
+      tempore ad molestias id quia beatae quisquam libero dolorem provident.
+      Non, obcaecati dolores officia fugiat quidem tempora fugit tempore,
+      consequatur ipsa nulla nostrum velit quam maxime voluptas, labore impedit!
+      Necessitatibus repellat rem perferendis error odio quod tenetur,
+      dignissimos dolorem harum reprehenderit quam veniam laboriosam itaque
+      eveniet maiores quo et facilis debitis eligendi similique quibusdam nemo
+      ut. Alias beatae inventore fugit, voluptatum nemo illum soluta sed
+      voluptate perspiciatis animi corrupti, possimus aperiam odio provident.
+    </div>
+  );
+}
