d81657b8525ed27b56c7e16708beb8daadf045a8
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-08-24 12:24:09 +0100

    i made the react slider responsive

--- src/pages/SimpleSlider.tsx
+++ src/pages/SimpleSlider.tsx
@@ -1,86 +1,3 @@
-// import React from "react";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import Slider from "react-slick";
-// import "../App.css"; // Import App.css
-
-// export default function SimpleSlider() {
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: true, // Show navigation arrows
-//   };
-
-//   return (
-//     <div className="relative w-1/4 mx-auto mt-8">
-//       <Slider {...settings}>
-//         <div className="flex items-center justify-center h-48 bg-blue-500 text-white text-2xl font-bold">
-//           <figure>
-//             <img
-//               src="/images/hero image.jpeg"
-//               alt="trainer-one"
-//               className="w-full"
-//             />
-//           </figure>
-//         </div>
-//         <div className="flex items-center justify-center h-48 bg-green-500 text-white text-2xl font-bold">
-//           <figure>
-//             <img
-//               src="/images/trainer-two.jfif"
-//               alt="trainer-two"
-//               className="w-full"
-//             />
-//           </figure>
-//         </div>
-//         <div className="flex items-center justify-center h-48 bg-red-500 text-white text-2xl font-bold">
-//           <figure>
-//             <img
-//               src="/images/trainer-three.jfif"
-//               alt="trainer-three"
-//               className="w-full"
-//             />
-//           </figure>
-//         </div>
-//         <div className="flex items-center justify-center h-48 bg-purple-500 text-white text-2xl font-bold">
-//           <figure>
-//             <img
-//               src="/images/trainer-four.jfif"
-//               alt="trainer-four"
-//               className="w-full"
-//             />
-//           </figure>
-//         </div>
-//         <div className="flex items-center justify-center h-48 bg-yellow-500 text-white text-2xl font-bold">
-//           <figure>
-//             <img
-//               src="/images/trainer-five.jfif"
-//               alt="trainer-five"
-//               className="w-full"
-//             />
-//           </figure>
-//         </div>
-//         <div className="flex items-center justify-center h-48 bg-pink-500 text-white text-2xl font-bold">
-//           <figure>
-//             <img
-//               src="/images/trainer-six.jfif"
-//               alt="trainer-six"
-//               className="w-full"
-//             />
-//           </figure>
-//         </div>
-//       </Slider>
-//     </div>
-//   );
-// }
-
-
-
-
-
-
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -108,59 +25,59 @@ const SimpleSlider: React.FC = () => {
   };
 
   return (
-    <div className="relative w-1/4 mx-auto mt-8">
+    <div className="relative w-full md:w-3/4 lg:w-1/2 mx-auto mt-8">
       <Slider {...settings}>
-        <div className="flex items-center justify-center h-48 bg-blue-500 text-white text-2xl font-bold">
-          <figure>
+        <div className="flex items-center justify-center h-48 md:h-64 lg:h-80 bg-blue-500 text-white text-2xl font-bold">
+          <figure className="w-full h-full">
             <img
               src="/images/hero image.jpeg"
               alt="trainer-one"
-              className="w-full"
+              className="w-full h-full object-cover"
             />
           </figure>
         </div>
-        <div className="flex items-center justify-center h-48 bg-green-500 text-white text-2xl font-bold">
-          <figure>
+        <div className="flex items-center justify-center h-48 md:h-64 lg:h-80 bg-green-500 text-white text-2xl font-bold">
+          <figure className="w-full h-full">
             <img
               src="/images/trainer-two.jfif"
               alt="trainer-two"
-              className="w-full"
+              className="w-full h-full object-cover"
             />
           </figure>
         </div>
-        <div className="flex items-center justify-center h-48 bg-red-500 text-white text-2xl font-bold">
-          <figure>
+        <div className="flex items-center justify-center h-48 md:h-64 lg:h-80 bg-red-500 text-white text-2xl font-bold">
+          <figure className="w-full h-full">
             <img
               src="/images/trainer-three.jfif"
               alt="trainer-three"
-              className="w-full"
+              className="w-full h-full object-cover"
             />
           </figure>
         </div>
-        <div className="flex items-center justify-center h-48 bg-purple-500 text-white text-2xl font-bold">
-          <figure>
+        <div className="flex items-center justify-center h-48 md:h-64 lg:h-80 bg-purple-500 text-white text-2xl font-bold">
+          <figure className="w-full h-full">
             <img
               src="/images/trainer-four.jfif"
               alt="trainer-four"
-              className="w-full"
+              className="w-full h-full object-cover"
             />
           </figure>
         </div>
-        <div className="flex items-center justify-center h-48 bg-yellow-500 text-white text-2xl font-bold">
-          <figure>
+        <div className="flex items-center justify-center h-48 md:h-64 lg:h-80 bg-yellow-500 text-white text-2xl font-bold">
+          <figure className="w-full h-full">
             <img
               src="/images/trainer-five.jfif"
               alt="trainer-five"
-              className="w-full"
+              className="w-full h-full object-cover"
             />
           </figure>
         </div>
-        <div className="flex items-center justify-center h-48 bg-pink-500 text-white text-2xl font-bold">
-          <figure>
+        <div className="flex items-center justify-center h-48 md:h-64 lg:h-80 bg-pink-500 text-white text-2xl font-bold">
+          <figure className="w-full h-full">
             <img
               src="/images/trainer-six.jfif"
               alt="trainer-six"
-              className="w-full"
+              className="w-full h-full object-cover"
             />
           </figure>
         </div>
