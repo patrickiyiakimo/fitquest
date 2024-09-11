4cc2f12ada4e32b41b5115e8c8ac175fdbbc658e
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-09-11 01:30:10 +0100

    made the mobile device responsive

--- src/components/Membership.tsx
+++ src/components/Membership.tsx
@@ -13,13 +13,13 @@ export default function Membership() {
   return (
     <div className="font-mont">
       <div>
-        <h1 className="text-xl md:text-2xl lg:text-6xl text-center font-bold mt-40 mb-32">
+        <h1 className="text-xl md:text-2xl lg:text-6xl text-center mt-32 font-bold md:mt-40 mb-14 md:mb-32">
           Register Here
         </h1>
       </div>
-      <div className="flex ml-40">
-        <div>
-          <h2 className="font-bold text-4xl mb-10">Contact Information</h2>
+      <div className="md:flex md:ml-40">
+        <div className="ml-5">
+          <h2 className="font-bold text-xl lg:text-4xl mb-3   lg:mb-10">Contact Information</h2>
           <p className="mb-20">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum esse
             laborum assumenda fugit mollitia delectus minus perspiciatis dicta
@@ -27,7 +27,7 @@ export default function Membership() {
             ducimus nisi rem.
           </p>
           <div>
-            <div className="grid grid-cols-2 gap-10">
+            <div className="grid grid-cols-2 mb-10 gap-10">
               <span>
                 <IoLocationOutline className="w-20 h-20" />
                 <p className="font-semibold text-xl pb-2">Our Address</p>
@@ -57,42 +57,42 @@ export default function Membership() {
           </div>
         </div>
         <div>
-          <div className="card bg-base-100 w-96 shadow-2xl ml-10 mr-40">
-            <h2 className="font-bold text-3xl pl-10 mb-10 mt-14 whitespace-nowrap">
+          <div className="card ml-5 mr-5 bg-base-100 lg:w-96 shadow-2xl lg:ml-10 lg:mr-40">
+            <h2 className="font-bold ml-20 text-xl lg:text-3xl lg:pl-10 mb-10 mt-14 whitespace-nowrap">
               Book An Appointment
             </h2>
             <form>
               <input
                 type="text"
                 placeholder="Your Name"
-                className=" border-2 py-3 ml-7 mb-8 px-4 w-80 rounded-md "
+                className=" border-2 py-3 w-72 ml-10 lg:ml-7 mb-8 px-4 lg:w-80 rounded-md "
               />
               <input
                 type="text"
                 placeholder="Your Email"
-                className=" border-2 py-3 ml-7 mb-8 px-4 w-80 rounded-md "
+                className=" border-2 py-3 w-72 ml-10 lg:ml-7 mb-8 px-4 lg:w-80 rounded-md "
               />
               <input
                 type="text"
                 placeholder="Select Subject"
-                className=" border-2 py-3 ml-7 mb-8 px-4 w-80 rounded-md "
+                className=" border-2 py-3 w-72 ml-10 lg:ml-7 mb-8 px-4 lg:w-80 rounded-md "
               />
               <textarea
                 placeholder="Send us a message"
-                className=" border-2 py-3 ml-7 mb-8 px-4 w-80 rounded-md "
+                className=" border-2 py-3 w-72 ml-10 lg:ml-7 mb-8 px-4 lg:w-80 rounded-md "
               ></textarea>
-              <button className=" bg-orange-600 hover:bg-orange-500 text-white mb-20 w-80 py-3 m-4 ml-7 rounded-md">
+              <button className="w-72 ml-10 bg-orange-600 hover:bg-orange-500 text-white mb-20 lg:w-80 py-3 m-4 lg:ml-7 rounded-md">
                 Book Appointment
               </button>
             </form>
           </div>
         </div>
       </div>
-      <div className="ml-40">
-        <p className="font-semibold py-10 text-xl">
+      <div className="lg:ml-40">
+        <p className="font-semibold ml-5 py-10 text-xl">
           Follow us on our social media handles:
-          <div className="flex gap-14 mt-10">
-            <p className="px-5 py-5 rounded-md bg-gray-400 hover:bg-gray-300 cursor-pointer text-orange-800">
+          <div className="flex  gap-10 lg:gap-14 mt-10">
+            <p className="px-5 py-5 rounded-md bg-gray-400 hover:bg-gray-300 cursor-pointer text-orange-800 ">
               <FaFacebookF />
             </p>
             <p className="px-5 py-5 rounded-md bg-gray-400 hover:bg-gray-300 cursor-pointer text-orange-800">
