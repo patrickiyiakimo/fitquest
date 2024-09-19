54d219cb3add4bb908d6283236389e518b49838a
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-09-19 23:26:50 +0100

    updated

--- src/pages/SimpleSlider.tsx
+++ src/pages/SimpleSlider.tsx
@@ -21,7 +21,7 @@ const SimpleSlider: React.FC = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true, // Show navigation arrows
+    arrows: true, 
   };
 
   return (
